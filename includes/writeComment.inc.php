39ff3827ac7a018b7e0c2ec0d17c4f30484467ae
Benoît <bgrandval@deastanceservices.fr>
2024-03-20 14:59:32 +0100

    Correction

--- includes/writeComment.inc.php
+++ includes/writeComment.inc.php
@@ -10,8 +10,7 @@ $commentaire = mysqli_real_escape_string($connexion, $_POST['commentaire']);
 
 // Insertion des données du formulaire dans la DB
 
-$commentSQL = "INSERT INTO `commentaires` ( `username`, `commentaire`) VALUES
-('$username', '$commentaire');";
+$commentSQL = "INSERT INTO `commentaires` (`username`, `commentaire`) VALUES ('$username', '$commentaire');";
 
 // '$dateComment' pas ouf préférer à la place la fonction SQL NOW() !!!
 //  NOW() pas ouf préférer le CURRENT_TIMESTAMP dans les attributs de l'objet
