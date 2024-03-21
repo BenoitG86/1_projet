08b226d95d1d824051b0233017fb418e12def582
Benoît <bgrandval@deastanceservices.fr>
2024-03-21 13:21:13 +0100

    Mise en forme et correction

--- includes/showComment.inc.php
+++ includes/showComment.inc.php
@@ -5,10 +5,6 @@ include 'dbconnect.inc.php';
 // Passage de la date en français
 
 setlocale(LC_TIME, 'fr_FR', 'French');
-// setlocale(LC_ALL, 'fr_FR.UTF8', 'fr_FR','fr','fr','fra','fr_FR@euro');
-// date_default_timezone_set('Europe/Paris');
-
-
 
 // Executer une requête
 
@@ -19,22 +15,22 @@ $readCommentCheck = mysqli_num_rows($readComment);
 
 // Afficher une requête
 
-if ($readCommentCheck > 0) {
-      while ($row = mysqli_fetch_assoc($readComment)) {
-            $time = strtotime($row['dateCommentaire']);
+// if ($readCommentCheck > 0) {
+//       while ($row = mysqli_fetch_assoc($readComment)) {
+//             $time = strtotime($row['dateCommentaire']);
             
-            // Passage de la date en français - bis
+//             // Passage de la date en français - bis
 
-            $date =  str_replace(
-                  ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-                  ["Lundi", "Mardi", "Mercredi", "Jeudi", "Jeudi", "Samedi", "Dimanche", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre"],
-                  date('l d F Y à H:i', $time)
-            );
-            echo '<div>' . $row['username'] . ' a écrit [le ' . $date . '] : <p>' . $row['commentaire'] . '</p></div>';
-      }
-} else {
-      echo "Erreur : " . mysqli_error($connexion);
-}
+//             $date =  str_replace(
+//                   ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+//                   ["Lundi", "Mardi", "Mercredi", "Jeudi", "Jeudi", "Samedi", "Dimanche", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre"],
+//                   date('l d F Y à H:i', $time)
+//             );
+//             echo '<div>' . $row['username'] . ' a écrit [le ' . $date . '] : <p>' . $row['commentaire'] . '</p></div>';
+//       }
+// } else {
+//       echo "Erreur : " . mysqli_error($connexion);
+// }
 
 
 // -------------- à retirer avant mise en prod ------------
--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 // Connexion
 
 include 'includes/dbconnect.inc.php';
+include 'includes/showComment.inc.php';
 
 // Vérifier la connexion
 
 if (!$connexion) {
       die("Échec de la connexion : " . mysqli_connect_error());
 } else {
-      echo "Connexion réussie à la base de données.";
+      echo '<script>console.log("Connexion réussie à la base de données. GG ma gueule");</script>';
 }
+
+
 
 ?>
 
@@ -21,34 +24,60 @@ if (!$connexion) {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=0, initial-scale=1.0">
       <link rel="stylesheet" href="styles/style.css">
+      <link rel="shortcut icon" href="icons/logo-wechat.svg" type="image/x-icon">
       <!-- <link rel="stylesheet" href="styles/reset.css"> -->
-      <title>Document</title>
+      <title>Lunar chatboard</title>
+
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Carrois+Gothic+SC&family=Dr+Sugiyama&display=swap" rel="stylesheet">
 </head>
 
 <body>
+      <main>
+            <h1>Lunar chatboard</h1>
+            <div class="earth">
+                  <form action="includes/writeComment.inc.php" method="post">
+                        <div>
+                              <label for="username">Votre nom d'utilisateur </label>
+                              <input type="text" name="username" id="username" placeholder="ici... !">
+                        </div>
+                        <div>
+                              <label for="commentaire">Ecrivez à la lune</label>
+                              <textarea name="commentaire" id="commentaire" cols="50" rows="10" placeholder="Entrez ici votre commentaire... !"></textarea>
+                        </div>
+                        <div>
+                              <input type="submit">
+                              <input type="reset">
+                        </div>
+                  </form>
+            </div>
+            <div class="moon">
 
-      <div class="earth">
-            <h1>Je suis un titre très très important ohlalala</h1>
-            <form action="includes/writeComment.inc.php" method="post">
-                  <div>
-                        <label for="username">Votre nom d'utilisateur </label>
-                        <input type="text" name="username" id="username" placeholder="ici... !">
-                  </div>
-                  <div>
-                        <label for="commentaire">Ecrivez à la lune</label>
-                        <textarea name="commentaire" id="commentaire" cols="50" rows="10" placeholder="Entrez ici votre commentaire... !"></textarea>
-                  </div>
-                  <div>
-                        <input type="submit">
-                        <input type="reset">
-                  </div>
-            </form>
-      </div>
-      <div class="moon">
-            <form action="includes/showComment.inc.php" method="post">
-                  <label for="submit">Afficher les commentaires <input id="submit" type="submit"></label>
-            </form>
-      </div>
+                  <?php if ($readCommentCheck > 0) {
+                        while ($row = mysqli_fetch_assoc($readComment)) {
+                              $time = strtotime($row['dateCommentaire']);
+
+                              // Passage de la date en français - bis
+
+                              $date =  str_replace(
+                                    ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                                    ["Lundi", "Mardi", "Mercredi", "Jeudi", "Jeudi", "Samedi", "Dimanche", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre"],
+                                    date('l d F Y à H:i', $time)
+                              );
+                              echo '<div class="comment"><div class="info">' . $row['username'] . ' a écrit [le ' . $date . '] : </div><p>' . $row['commentaire'] . '</p></div>';
+                        }
+                  } else {
+                        echo "Erreur : " . mysqli_error($connexion);
+                  }
+                  ?>
+
+            </div>
+      </main>
+      <footer>
+            <p>Je suis une petite note de bas de page.</p>
+            <a href="https://github.com/BenoitG86/1_projet"><img src="icons/logo-github.svg" alt="" class="iconFooter"> Retrouvez-ici le GitHub de ce projet</a>
+      </footer>
 </body>
 
 </html>
