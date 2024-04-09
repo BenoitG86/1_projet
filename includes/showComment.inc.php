df62d91ea25d082fbbf8250ba337a9629839ebd2
Benoît <bgrandval@deastanceservices.fr>
2024-04-09 14:03:33 +0200

    Suppression commentaires inutiles

--- includes/showComment.inc.php
+++ includes/showComment.inc.php
@@ -12,30 +12,6 @@ $showSQL = "SELECT * FROM commentaires";
 $readComment = mysqli_query($connexion, $showSQL);
 $readCommentCheck = mysqli_num_rows($readComment);
 
-
-// Afficher une requête
-
-// if ($readCommentCheck > 0) {
-//       while ($row = mysqli_fetch_assoc($readComment)) {
-//             $time = strtotime($row['dateCommentaire']);
-            
-//             // Passage de la date en français - bis
-
-//             $date =  str_replace(
-//                   ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-//                   ["Lundi", "Mardi", "Mercredi", "Jeudi", "Jeudi", "Samedi", "Dimanche", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre"],
-//                   date('l d F Y à H:i', $time)
-//             );
-//             echo '<div>' . $row['username'] . ' a écrit [le ' . $date . '] : <p>' . $row['commentaire'] . '</p></div>';
-//       }
-// } else {
-//       echo "Erreur : " . mysqli_error($connexion);
-// }
-
-
-// -------------- à retirer avant mise en prod ------------
-// header("Location: ../index.php?read=success");
-
 // Fermer la connexion
 
 mysqli_close($connexion);
